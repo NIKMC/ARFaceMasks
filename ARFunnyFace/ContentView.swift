2a3ae695397537250862d9e036fe4bdef99461a9
NIKMC <nikmc10@gmail.com>
2023-06-08 10:28:43 +0300

    add anchors and new mask for robot

--- ARFunnyFace/ContentView.swift
+++ ARFunnyFace/ContentView.swift
@@ -7,30 +7,143 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
+
+var arView: ARView!
 
 struct ContentView : View {
+    
+    // MARK: - Properties
+    
+    @State private var propId: Int = 0
+    @State private var isFrontCamera: Bool = false
+    // MARK: - Body
+    
     var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
+        // 1
+        ZStack {
+            ARViewContainer(propId: $propId, isFrontCamera: $isFrontCamera).edgesIgnoringSafeArea(.all)
+
+            VStack {
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        self.isFrontCamera.toggle()
+                    }) {
+                        Image(systemName:  "arrow.triangle.2.circlepath.camera")
+                            .font(.system(size: 40, weight: .ultraLight))
+                            .foregroundColor(.gray)
+                    }
+                }
+                Spacer()
+                
+                
+                // 3
+                HStack {
+                    
+                    Spacer()
+                    Button(action: {
+                        self.propId = self.propId <= 0 ? 0 : self.propId - 1
+                    }) {
+                        Image("PreviousButton").clipShape(Circle())
+                    }
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        
+                        self.takeSnapshot()
+                    }) {
+                        Image("ShutterButton").clipShape(Circle())
+                    }
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        self.propId = self.propId >= 2 ? 2 : self.propId + 1
+                    }) {
+                        Image("NextButton").clipShape(Circle())
+                    }
+                    Spacer()
+                }
+            }
+            
+        }
+        
     }
+    
+    func takeSnapshot() {
+        // 1
+          arView.snapshot(saveToHDR: false) { (image) in
+            // 2
+            let compressedImage = UIImage(
+              data: (image?.pngData())!)
+        // 3
+            UIImageWriteToSavedPhotosAlbum(
+              compressedImage!, nil, nil, nil)
+        }
+    }
+    
 }
 
 struct ARViewContainer: UIViewRepresentable {
     
+    // MARK: - Properties
+    
+    @Binding var propId: Int
+    @Binding var isFrontCamera: Bool
+    
+    // MARK: - Functions
+    
     func makeUIView(context: Context) -> ARView {
         
-        let arView = ARView(frame: .zero)
+        arView = ARView(frame: .zero)
         
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+//        let boxAnchor = try! Experience.loadEyes()
         
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+//        arView.scene.anchors.append(boxAnchor)
         
         return arView
         
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func updateUIView(_ uiView: ARView, context: Context) {
+        uiView.scene.anchors.removeAll()
+        if isFrontCamera {
+            let arConfiguration = ARFaceTrackingConfiguration()
+            uiView.session.run(arConfiguration, options: [.resetTracking, .removeExistingAnchors])
+            
+            switch propId {
+            case 0:
+                guard let arAnchor = try? Experience.loadEyes() else { break
+                }
+                uiView.scene.anchors.append(arAnchor)
+                        
+            case 1:
+                guard let arAnchor = try? Experience.loadGlasses() else { break
+                }
+                uiView.scene.anchors.append(arAnchor)
+            case 2:
+                guard let arAnchor = try? Experience.loadMustache() else { break
+                }
+                uiView.scene.anchors.append(arAnchor)
+            default:
+                break
+            }
+        } else {
+            let arConfiguration = ARWorldTrackingConfiguration()
+            arConfiguration.planeDetection = .horizontal
+            uiView.session.run(arConfiguration, options: [.resetTracking, .removeExistingAnchors])
+            
+                guard let arAnchor = try? Experience.loadFurniture() else {
+                    print("Not detected arAnchor")
+                    return
+                }
+                uiView.scene.anchors.append(arAnchor)
+        }
+    }
     
 }
 
